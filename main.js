e1cad471e8340ab6e2342b2599b8140e16a04b4d
Junseok <narazzo@naver.com>
2022-10-18 17:18:08 +0900

    js scrolling modify

--- main.js
+++ main.js
@@ -4,22 +4,29 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeight) {
+if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
-  } else {
+} else {
     navbar.classList.remove('navbar--dark');
-  }
+}
 });
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link == null) {
+const target = event.target;
+const link = target.dataset.link;
+if (link == null) {
     return;
-  }
-
-  const scrollTo = document.querySelector(link);
-scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+scrollIntoView(link);
 });
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+  }
